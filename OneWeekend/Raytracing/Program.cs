8ec74c25f116ba547aaed5e124f3bf45b3269762
pjmlp <pjmlp@progtools.org>
2022-01-06 19:41:07 +0100

    Adding chapter 4.

--- OneWeekend/Raytracing/Program.cs
+++ OneWeekend/Raytracing/Program.cs
@@ -1,9 +1,21 @@
 ﻿using RaytracingUtils;
 using System.Numerics;
 
+static bool HitSphere(in Vector3 center, float radius, Ray r)
+{
+    Vector3 oc = r.Origin - center;
+    float a = Vector3.Dot(r.Direction, r.Direction);
+    float b = 2.0f * Vector3.Dot(oc, r.Direction);
+    float c = Vector3.Dot(oc, oc) - radius * radius;
+    float discriminat = b * b - 4 * a * c;
+    return (discriminat > 0);
+}
 
 static Vector3 RayColor(Ray r)
 {
+    if (HitSphere(new Vector3(0.0f, 0.0f, -1.0f), 0.5f, r))
+        return Vector3.UnitX;
+
     Vector3 unitDirection = Vector3.Normalize(r.Direction);
     float t = 0.5f * (unitDirection.Y + 1.0f);
     return (1.0f - t) * Vector3.One + t * new Vector3(0.5f, 0.7f, 1.0f);
@@ -17,7 +29,7 @@ var imageBuffer = new byte[imageWidth * imageHeight * bytesPerPixel];
 
 var lowerLeftCorner = new Vector3(-2.0f, -1.0f, -1.0f);
 var horizontal = new Vector3(4.0f, 0.0f, 0.0f);
-var vertical = new Vector3(0.0f, 0.2f, 0.0f);
+var vertical = new Vector3(0.0f, 2.0f, 0.0f);
 var origin = new Vector3(0.0f, 0.0f, 0.0f);
 
 for (int j = imageHeight - 1, y = 0; j >= 0; j--, y++)
@@ -38,10 +50,9 @@ for (int j = imageHeight - 1, y = 0; j >= 0; j--, y++)
         imageBuffer[current] = ir;
         imageBuffer[current + 1] = ig;
         imageBuffer[current + 2] = ib;
-
-        //Console.WriteLine($"{ir} {ig} {ib} => {i} {y} {current} => {(y * imageWidth * bytesPerPixel)} {i * bytesPerPixel}");
     }
 }
+
 
 
 ImageWriter.SaveAsBmp("demo.bmp", imageBuffer, imageWidth, imageHeight);
