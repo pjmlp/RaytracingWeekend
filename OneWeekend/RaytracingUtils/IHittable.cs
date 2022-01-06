4d3aa972ae9cbcd0171d1b3aa0aa6aa335799917
pjmlp <pjmlp@progtools.org>
2022-01-06 19:46:36 +0100

    Renaming file due to typo.

---
+++ OneWeekend/RaytracingUtils/IHittable.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace RaytracingUtils;
+
+public struct HitRecord
+{
+    public float t;
+
+    public Vector3 p;
+
+    public Vector3 Normal;
+
+    public bool FrontFace;
+
+    public void SetFaceNormal(Ray r, in Vector3 outwardNormal) {
+        FrontFace = Vector3.Dot(r.Direction, outwardNormal) < 0;
+        Normal = FrontFace ? outwardNormal : -outwardNormal;
+    }
+}
+
+public interface IHittable
+{
+    bool Hit(Ray r, float tMin, float tMax, ref HitRecord rec);
+}
+
+public class HittableList : IHittable
+{
+    private IList<IHittable> objects;
+
+    public HittableList()
+    {
+        objects = new List<IHittable>();
+    }
+
+    public void Clear()
+    {
+        objects.Clear();
+    }
+
+    public void Add(IHittable obj)
+    {
+        objects.Add(obj);
+    }
+
+    public bool Hit(Ray r, float tMin, float tMax, ref HitRecord rec)
+    {
+        HitRecord temp_rec = default;
+        bool hitAnything = false;
+        var closestSoFar = tMax;
+
+        foreach (var obj in objects) {
+            if (obj.Hit(r, tMin, closestSoFar, ref temp_rec))
+            {
+                hitAnything = true;
+                closestSoFar = temp_rec.t;
+                rec = temp_rec;
+            }
+        }
+
+        return hitAnything;
+    }
+}
