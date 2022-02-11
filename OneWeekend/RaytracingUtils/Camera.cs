d5010c76c9a485ed291051cfe1899f00badcbe87
pjmlp <pjmlp@progtools.org>
2022-02-12 00:17:40 +0100

    Fixing the names to follow .NET conventions.

--- OneWeekend/RaytracingUtils/Camera.cs
+++ OneWeekend/RaytracingUtils/Camera.cs
@@ -14,15 +14,15 @@ public class Camera
 
     public Camera()
     {
-        var aspect_ratio = 16.0f / 9.0f;
-        var viewport_height = 2.0f;
-        var viewport_width = aspect_ratio * viewport_height;
-        var focal_length = 1.0f;
+        var aspectRatio = 16.0f / 9.0f;
+        var viewportHeight = 2.0f;
+        var viewportWidth = aspectRatio * viewportHeight;
+        var focalLength = 1.0f;
 
         origin = new Vector3(0, 0, 0);
-        horizontal = new Vector3(viewport_width, 0.0f, 0.0f);
-        vertical = new Vector3(0.0f, viewport_height, 0.0f);
-        lower_left_corner = origin - horizontal / 2 - vertical / 2 - new Vector3(0.0f, 0.0f, focal_length);
+        horizontal = new Vector3(viewportWidth, 0.0f, 0.0f);
+        vertical = new Vector3(0.0f, viewportHeight, 0.0f);
+        lower_left_corner = origin - horizontal / 2 - vertical / 2 - new Vector3(0.0f, 0.0f, focalLength);
     }
 
     public Ray GetRay(double u, double v)
