ef7c3f286622b6bec4b604dccb8910c0697c5dc7
pjmlp <pjmlp@progtools.org>
2022-03-13 17:03:03 +0100

    Chapter 10.4, Schlick Approximation

--- OneWeekend/RaytracingUtils/Material.cs
+++ OneWeekend/RaytracingUtils/Material.cs
@@ -65,21 +65,30 @@ public class Dielectric : IMaterial
         attenuation = new Vector3(1.0f, 1.0f, 1.0f);
         float refractionRatio = rec.FrontFace ? (1.0f / ir) : ir;
 
-        Vector3 unit_direction = Vector3.Normalize(rIn.Direction);
+        Vector3 unitDirection = Vector3.Normalize(rIn.Direction);
 
-        float cos_theta = MathF.Min(Vector3.Dot(-unit_direction, rec.Normal), 1.0f);
-        float sin_theta = MathF.Sqrt(1.0f - cos_theta*cos_theta);
+        float cosTheta = MathF.Min(Vector3.Dot(-unitDirection, rec.Normal), 1.0f);
+        float sinTheta = MathF.Sqrt(1.0f - cosTheta*cosTheta);
 
-        bool cannot_refract = refractionRatio * sin_theta > 1.0;
+        bool cannotRefract = refractionRatio * sinTheta > 1.0;
         Vector3 direction;
 
-        if (cannot_refract)
-            direction = Reflect(unit_direction, rec.Normal);
+        if (cannotRefract || Reflectance(cosTheta, refractionRatio) > RandomDouble())
+            direction = Reflect(unitDirection, rec.Normal);
         else
-            direction = Refract(unit_direction, rec.Normal, refractionRatio);
+            direction = Refract(unitDirection, rec.Normal, refractionRatio);
 
         scattered = new Ray(rec.p, direction);
 
         return true;
+    }
+
+    private double Reflectance(float cosine, float refIdx)
+    {
+        // Use Schlick's approximation for reflectance.
+        var r0 = (1-refIdx) / (1+refIdx);
+        r0 = r0*r0;
+
+        return r0 + (1.0f - r0) * MathF.Pow((1.0f - cosine),5.0f);
     }
 }
